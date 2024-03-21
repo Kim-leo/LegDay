a34333c648516786bde2bff4a52d7385ac59fdd7
Kim-leo <ochell2@naver.com>
2024-03-21 13:59:57 +0900

    Initial Commit

---
+++ LegDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LegDay
+//
+//  Created by 김승현 on 3/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
