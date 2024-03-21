26df7c419b4415f23e8715f5a9c0e8b72a87bed9
Kim-leo <ochell2@naver.com>
2024-03-21 15:01:04 +0900

    탭바 컨트롤러 제작 및 화면 연동

---
+++ LegDay/Controllers/WorkoutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorkoutViewController.swift
+//  LegDay
+//
+//  Created by 김승현 on 3/21/24.
+//
+
+import UIKit
+
+class WorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
