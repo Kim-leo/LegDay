ab1f996ac7a3875fc9be5f58a85c22f76f4eead7
Kim-leo <ochell2@naver.com>
2024-03-21 15:14:31 +0900

    뷰, 뷰모델 파일 생성

--- LegDay/Views/SettingView.swift
+++ LegDay/Views/SettingView.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UIKit
+
+class SettingView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+//        self.addSubview(startWorkoutBtn)
+        self.backgroundColor = .white
+        viewLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("뷰 초기화 에러 발생")
+    }
+    
+    func viewLayout() {
+        
+    }
+}
