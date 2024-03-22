317039a3207feeafa900532906cf33b8bdd496bb
Kim-leo <ochell2@naver.com>
2024-03-22 19:00:58 +0900

    SettingViewController, SettingView, SettingViewModel 기능 제작

---
+++ LegDay/Views/CollectionViewCells/LowerCell.swift
@@ -0,0 +1,17 @@
+//
+//  LowerCell.swift
+//  LegDay
+//
+//  Created by 김승현 on 3/22/24.
+//
+
+import UIKit
+
+class LowerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ LegDay/Views/CollectionViewCells/UpperCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpperCell.swift
+//  LegDay
+//
+//  Created by 김승현 on 3/22/24.
+//
+
+import UIKit
+
+class UpperCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
