80ebaeedc3430a299889c0ab0d88b4929a127f65
Kim-leo <ochell2@naver.com>
2024-03-25 03:50:41 +0900

    MyWorkout 화면 - 마이 운동 세트 표시될 테이블뷰 생성

---
+++ LegDay/Views/MyWorkoutTableViewCell/MyWorkoutCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyWorkoutCell.swift
+//  LegDay
+//
+//  Created by 김승현 on 3/25/24.
+//
+
+import UIKit
+
+class MyWorkoutCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
