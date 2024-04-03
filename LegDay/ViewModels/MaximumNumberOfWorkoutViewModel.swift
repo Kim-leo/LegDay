a5f95ea422027dd045cbb1af055553f39c7a297a
Kim-leo <ochell2@naver.com>
2024-04-03 13:47:28 +0900

    운동 횟수 조절가능한 화면 (VC, View, ViewModel) 기본 형태 제작

---
+++ LegDay/ViewModels/MaximumNumberOfWorkoutViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MaximumNumberOfWorkoutViewModel.swift
+//  LegDay
+//
+//  Created by 김승현 on 4/3/24.
+//
+
+import Foundation
---
+++ LegDay/Views/MaximumNumberOfWorkoutView.swift
@@ -0,0 +1,8 @@
+//
+//  MaximumNumberOfWorkoutView.swift
+//  LegDay
+//
+//  Created by 김승현 on 4/3/24.
+//
+
+import Foundation
