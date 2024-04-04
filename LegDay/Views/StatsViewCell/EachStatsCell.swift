9e3154e405349289658cdef0a76abe94d1faf89c
Kim-leo <ochell2@naver.com>
2024-04-04 14:16:21 +0900

    운동 기록 통계 화면 파일 생성( ViewController, View, ViewModel)

---
+++ LegDay/Views/StatsViewCell/EachStatsCell.swift
@@ -0,0 +1,17 @@
+//
+//  EachStatsCell.swift
+//  LegDay
+//
+//  Created by 김승현 on 4/4/24.
+//
+
+import UIKit
+
+class EachStatsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ LegDay/Views/StatsViewCell/maxNumCell.swift
@@ -0,0 +1,17 @@
+//
+//  maxNumCell.swift
+//  LegDay
+//
+//  Created by 김승현 on 4/4/24.
+//
+
+import UIKit
+
+class maxNumCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
